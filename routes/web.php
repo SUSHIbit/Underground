9490d63d45047905112f9d33bbd406ceea0444ea
Sushi Maru <paperquiffzz@gmail.com>
2025-03-06 20:44:20 +0800

    Updated folder ft Accessor and Lecturer Functions

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,6 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/results/{attempt}', [ResultController::class, 'show'])->name('results.show');
 });
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
